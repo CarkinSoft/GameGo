b4bd6fd59a19427ae7f142a886a4147fe0f63001
CarkinOtter <carcardoza@csumb.edu>
2026-03-23 14:17:39 -0700

    Initial commit

---
+++ index.mjs
@@ -0,0 +1,16 @@
+import express from 'express';
+import axios from 'axios';
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+//routes
+//root route
+app.get('/', (req, res) => {
+   res.render('home.ejs', { });
+});
+
+app.listen(3000, () => {
+   console.log('server started');
+});
