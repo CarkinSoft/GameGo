40046e5f365ca0f5a71bcd60ea7b311076bd842f
Jacob Banducci <jbanducci321@gmail.com>
2026-03-25 08:53:40 -0700

    Added boiler plate database code

--- index.mjs
+++ index.mjs
@@ -1,15 +1,60 @@
 import express from 'express';
-
+import mysql from 'mysql2/promise';
 const app = express();
-app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+//for Express to get values using the POST method
+app.use(express.urlencoded({extended:true}));
+
+//setting up database connection pool, replace values in red
+const pool = mysql.createPool({
+    host: "k2pdcy98kpcsweia.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "gmhoccg2uue6ds29",
+    password: "vvjakq4zu7siihcs",
+    database: "nm2pf20notjcum1m",
+    connectionLimit: 10,
+    waitForConnections: true
+});
+
 
 //routes
-//root route
 app.get('/', (req, res) => {
    res.render('home.ejs', { });
 });
 
-app.listen(3000, () => {
-   console.log('server started');
-});
+// app.get('/searchByLikes', async (req, res) => {
+//     let startNum = req.query.startNum;
+//     let endNum = req.query.endNum;
+
+//     try {
+//         let sql = `SELECT quote, firstName, lastName, likes
+//             FROM quotes
+//             NATURAL JOIN authors
+//             WHERE likes BETWEEN ? AND ?
+//             ORDER BY likes`;
+//         let sqlParams = [startNum, endNum];;
+
+//         const [rows] = await pool.query(sql, sqlParams);
+//         res.render("quotesLikes.ejs", {rows});
+//     } catch (err) {
+//         console.error("Database error:", err);
+//         res.status(500).send("Database error!");
+//     }
+
+// });
+
+
+app.get("/dbTest", async(req, res) => {
+   try {
+        const [rows] = await pool.query("SELECT CURDATE()");
+        res.send(rows);
+    } catch (err) {
+        console.error("Database error:", err);
+        res.status(500).send("Database error!");
+    }
+});//dbTest
+
+app.listen(3000, ()=>{
+    console.log("Express server running")
+})
