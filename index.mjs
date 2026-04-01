8e731a398ad8ea3463aeadb2680755166c7591a7
Jacob Banducci <jbanducci321@gmail.com>
2026-03-31 17:10:35 -0700

    Added base logic for using RAWG api Users can search for games and add them to thier library

--- index.mjs
+++ index.mjs
@@ -26,6 +26,8 @@ const pool = mysql.createPool({
     waitForConnections: true
 });
 
+const RAWG_API_KEY = "ae5a2588f0c9456a914d874aeee6c546";
+
 
 // Landing page for the site (allows people to login or create an account)
 app.get('/', (req, res) => {
@@ -181,6 +183,119 @@ app.get("/dbTest", async (req, res) => {
         res.status(500).send("Database error!");
     }
 });//dbTest
+
+// search for games
+app.get('/searchGame', async (req, res) => {
+    if (!req.session.userId) { //makes sure user is logged in
+        return res.redirect('/');
+    }
+
+    let gameTitle = req.query.gameTitle;
+
+    try {
+        let url = `https://api.rawg.io/api/games?key=${RAWG_API_KEY}&search=${encodeURIComponent(gameTitle)}&page_size=10`;
+
+        let response = await fetch(url);
+        let data = await response.json();
+
+        res.render('searchResults.ejs', {gameTitle, games: data.results});
+
+    } catch (err) {
+        console.error("RAWG API error:", err);
+        res.status(500).send("RAWG API error!");
+    }
+});
+
+// route for displaying the game info
+app.get('/gameInfo', async (req, res) => {
+    if (!req.session.userId) {
+        return res.redirect('/');
+    }
+
+    let gameId = req.query.gameId;
+
+    try {
+        let url = `https://api.rawg.io/api/games/${gameId}?key=${RAWG_API_KEY}`;
+
+        let response = await fetch(url);
+        let game = await response.json();
+
+        res.render('game.ejs', { game });
+
+    } catch (err) {
+        console.error("RAWG API error:", err);
+        res.status(500).send("RAWG API error!");
+    }
+});
+
+// allows user to save game to their library
+app.post('/saveGame', async (req, res) => {
+    if (!req.session.userId) {
+        return res.json({
+            success: false,
+            message: "You must be logged in to save a game."
+        });
+    }
+
+    let rawgGameId = req.body.rawg_game_id;
+    let title = req.body.title;
+    let coverImage = req.body.cover_image;
+    let genres = req.body.genres;
+    let status = req.body.status;
+    let isFavorite = req.body.is_favorite ? 1 : 0;
+    let userId = req.session.userId;
+
+    try {
+        // starts by checking if the game exists (by rawg id) for that user in the table
+        let sql = `SELECT id
+                   FROM saved_games
+                   WHERE user_id = ?
+                   AND rawg_game_id = ?`;
+        let sqlParams = [userId, rawgGameId];
+
+        const [rows] = await pool.query(sql, sqlParams);
+
+        if (rows.length > 0) { // if the game already exists for the user then it is updated instead of newly inserted
+            sql = `UPDATE saved_games
+                   SET title = ?,
+                       cover_image = ?,
+                       genres = ?,
+                       status = ?,
+                       is_favorite = ?
+                   WHERE user_id = ?
+                   AND rawg_game_id = ?`;
+
+            sqlParams = [title, coverImage, genres, status, isFavorite, userId, rawgGameId];
+
+            await pool.query(sql, sqlParams);
+
+            return res.json({
+                success: true,
+                message: "Game updated successfully in your library!"
+            });
+        }
+
+        // if the game wasn't found then it is added and tied to the user
+        sql = `INSERT INTO saved_games
+               (user_id, rawg_game_id, title, cover_image, genres, status, is_favorite)
+               VALUES (?, ?, ?, ?, ?, ?, ?)`;
+        sqlParams = [userId, rawgGameId, title, coverImage, genres, status, isFavorite];
+
+        await pool.query(sql, sqlParams);
+
+        res.json({
+            success: true,
+            message: "Game saved successfully to your library!"
+        });
+
+    } catch (err) {
+        console.error("Database error:", err);
+        res.status(500).json({
+            success: false,
+            message: "Database error!"
+        });
+    }
+});
 
 
 
